31618c68c95bb82fa63d3426d1b4a814d0ba646c
ITStan <s.stanislavskaya@gmail.com>
2019-11-02 00:04:59 +0200

    Updates

---
+++ precache-manifest.cf37e3069d29a4225ef67f2b4a9fefc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c105b8632e5b9b650dc6ce8a1fd8a7f",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "6aab0255b0aca8d3ec0b",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "dc0d90f8ffd43125b2a1",
+    "url": "/react_tabs/static/js/2.ac5e6761.chunk.js"
+  },
+  {
+    "revision": "6aab0255b0aca8d3ec0b",
+    "url": "/react_tabs/static/js/main.b3bd2d41.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
